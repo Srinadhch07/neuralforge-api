b491a0b8243ed89c8b525a8ed2f2eb3593bc6453
Srinadhch07 <srinadhc07@gamil.com>
2026-03-05 11:32:17 +0530

    Debug: Uploading model to test deployment

--- zbackground.ps1
+++ zbackground.ps1
@@ -1,3 +1,3 @@
-# celery -A app.celery_app.celery worker --concurrency=4 --loglevel=info
+# celery -A app.config.celery_app.celery worker --pool=solo --loglevel=info --prefetch-multiplier=1
 celery -A app.config.celery_app.celery worker --pool=solo --loglevel=info
 Write-Host "Started worker ..." -ForegroundColor Green
